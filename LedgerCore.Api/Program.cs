1486e04c650df8de71320ebf4a009c0324f01f11
Super Washington Banga <superwbanga@gmail.com>
2026-06-19 09:28:52 +0000

    feat: add JWT provider and TimeProvider registration

--- LedgerCore.Api/Program.cs
+++ LedgerCore.Api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddSingleton(TimeProvider.System);
 
 builder.Services.AddApplication();
 
