0108b5ea398fae9fc7337bef3c5a45677dbcd5f8
Super Washington Banga <superwbanga@gmail.com>
2026-06-19 00:32:25 +0000

    feat: add database configurations for account and ledger transaction entities

---
+++ LedgerCore.Infrastructure/Database/Configurations/AccountConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class AccountConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.ToTable("Accounts");
+
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.AccountNumber)
+            .HasConversion(
+                accountNumber => accountNumber.Value,
+                value => new AccountNumber(value))
+            .HasColumnName("AccountNumber")
+            .HasMaxLength(10)
+            .IsRequired();
+
+        builder.HasIndex(a => a.AccountNumber).IsUnique();
+    }
+}
---
+++ LedgerCore.Infrastructure/Database/Configurations/LedgerTransactionConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class LedgerTransactionConfiguration : IEntityTypeConfiguration<LedgerTransaction>
+{
+    public void Configure(EntityTypeBuilder<LedgerTransaction> builder)
+    {
+        builder.ToTable("LedgerTransactions");
+
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Currency)
+            .HasConversion(
+                currency => currency.Value,
+                value => new CurrencyCode(value))
+            .HasColumnName("Currency")
+            .HasMaxLength(3)
+            .IsRequired();
+
+        builder.OwnsOne(t => t.AuditMeta);
+    }
+}
