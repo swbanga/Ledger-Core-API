745179050401e20270c5ea7fb82296f355f2a416
Super Washington Banga <superwbanga@gmail.com>
2026-06-20 20:07:10 +0000

    feat: add admin agent float provisioning

---
+++ LedgerCore.Domain/Constants/SystemAccountIds.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LedgerCore.Domain.Constants;
+
+public static class SystemAccountIds
+{
+    public static readonly Guid SystemRevenue = Guid.Parse("F0000000-0000-0000-0000-000000000001");
+    public static readonly Guid TaxLiabilityZimra = Guid.Parse("F0000000-0000-0000-0000-000000000002");
+    public static readonly Guid Settlement = Guid.Parse("F0000000-0000-0000-0000-000000000003");
+    public static readonly Guid Suspense = Guid.Parse("F0000000-0000-0000-0000-000000000004");
+}
